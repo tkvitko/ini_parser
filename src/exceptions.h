0db80e9985fbc1ad7644391387580708bc8547f6
Taras Kvitko <tkvitko@gmail.com>
2024-03-21 17:14:46 +0300

    bugfix

---
+++ src/exceptions.h
@@ -0,0 +1,60 @@
+//
+//  exceptions.h
+//  ini_parser
+//
+//  Created by tkvitko on 21.03.2024.
+//
+
+#ifndef exceptions_h
+#define exceptions_h
+
+class NoValueInLine : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "No value";
+    };
+};
+
+class BadValueLine : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Bad line with field";
+    };
+};
+
+class NoSuchSection : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "No such section";
+    };
+};
+
+class NoSuchFieldInSection : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "No such field in section";
+    };
+};
+
+class NoSuchFile : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "No such file";
+    };
+};
+
+class NotIntValue : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Value cant be represent of int";
+    };
+};
+
+class NotFloatValue : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Value cant be represent of int";
+    };
+};
+
+#endif /* exceptions_h */
